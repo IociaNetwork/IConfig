6013d6c8dd4a12de011d5d8bc4627976c5063e13
Mark Lisoway <IociaNetwork@gmail.com>
2017-05-03 20:17:52 -0600

    Added startup and shutdown methods

--- src/iocia/network/plugins/iconfig/IConfigMain.java
+++ src/iocia/network/plugins/iconfig/IConfigMain.java
@@ -3,4 +3,11 @@ package iocia.network.plugins.iconfig;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class IConfigMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {}
+    
+    @Override
+    public void onDisable() {}
+
 }
